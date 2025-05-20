ae0c3b18f676e4195b6d385cfed05e115d9c2b5f
Jhoann Sebastián Rueda Vanegas <jhoannrueda.thoughtworks@latam.com>
2025-05-20 09:08:40 -0500

    firebase service added to login page

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,42 +1,92 @@
 import { Button, Grid, Link, TextField } from "@mui/material";
 import { Google } from "@mui/icons-material";
 import { AuthLayout } from "../component/AuthLayout";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { loginUser } from "../services/AuthServices";
+
+
+interface LoginFormData {
+  email: string;
+  password: string;
+};
+
 
 export const LoginPage = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginFormData>();
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data: LoginFormData) => {
+    console.log("Login data:", data);
+
+    try {
+      const user = await loginUser(data.email, data.password);
+      if (user) {
+        console.log('Usuario logueado:', user);
+        navigate('/');
+      } else {
+        console.log('No se encontró el usuario en Firestore');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+
+  
   return (
-    <AuthLayout description="login Page">
-      <form>
+    <AuthLayout description="Login Page">
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <Grid sx={{ margin: 2 }}>
           <TextField
-            id="outlined-basic"
             label="Email"
             type="email"
-            variant="outlined"
             placeholder="email@mail.com"
             fullWidth
+            {...register("email", {
+              required: "El correo es obligatorio",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Correo no válido"
+              }
+            })}
+            error={!!errors.email}
+            helperText={errors.email?.message}
           />
         </Grid>
 
         <Grid sx={{ margin: 2 }}>
           <TextField
-            id="outlined-basic"
             label="Password"
             type="password"
-            variant="outlined"
-            placeholder="password"
+            placeholder="********"
             fullWidth
+            {...register("password", {
+              required: "La contraseña es obligatoria",
+              minLength: {
+                value: 6,
+                message: "Mínimo 6 caracteres"
+              }
+            })}
+            error={!!errors.password}
+            helperText={errors.password?.message}
           />
         </Grid>
 
         <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-          <Grid size={{ sm: 6, xs: 12 }}>
-            <Button variant="contained" fullWidth>
+          <Grid size={{xs:12 ,sm:6}}>
+            <Button type="submit" variant="contained" fullWidth>
               Login
             </Button>
           </Grid>
 
-          <Grid size={{ sm: 6, xs: 12 }}>
+          <Grid size={{xs:12 ,sm:6}}>
             <Button variant="contained" fullWidth startIcon={<Google />}>
               Google
             </Button>
@@ -44,7 +94,7 @@ export const LoginPage = () => {
 
           <Grid container direction="row" justifyContent="end">
             <Link component={RouterLink} color="inherit" to="/auth/register">
-              No tienes cuenta? Resgistrarse
+              No tienes cuenta? Registrarse
             </Link>
           </Grid>
         </Grid>
